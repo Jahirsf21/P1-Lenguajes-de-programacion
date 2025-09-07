1c7655d6ae164d14e83b9042dfe59a0e08a6be0c
Natalia01rg <natalia01rg@estudiantec.cr>
2025-09-07 12:17:33 -0600

    implementación de  funciones auxiliares

--- logicadenegocios/libros.c
+++ logicadenegocios/libros.c
@@ -1,1 +1,27 @@
 #include "../headers/libros.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//Funciones auxiliares
+int compararCadenas(const char *cadena1, const char *cadena2) {
+    while(*cadena1 && (cadena1 == cadena2)) {
+        cadena1++;
+        cadena2++;
+    }
+    return *(const unsigned char*)cadena1 - *(const unsigned char*)cadena2;
+}
+
+void copiarCadena(char *destino, const char *origen){
+    while(*origen) {
+        *destino++ = *origen++;
+    }
+    *destino = '\0';
+}
+
+int largoCadena(const char *cadena){
+    int largo = 0;
+    while (*cadena++) largo++;
+    return largo;
+}
