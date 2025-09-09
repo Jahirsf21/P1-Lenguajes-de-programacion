658ddf98b415859c4ef11e70d8756aa75e4e4676
sermontoya <sermontoya@estudiantec.cr>
2025-09-08 21:49:13 -0600

    Se crean los archivos de local

---
+++ headers/local.h
@@ -0,0 +1,26 @@
+#ifndef LOCAL_H
+#define LOCAL_H
+
+typedef struct usuario
+{
+    char* nombre;
+    char* password;
+} Usuario;
+
+typedef struct local {
+    char* nombre;
+    char* telefono;
+    char* cedulaJuridica;
+    char* horarioInicio;
+    char* horarioFin;
+    int sigPedido;
+    Usuario* usuario;
+} Local;
+
+extern Local *local;
+
+void cargarLocal();
+
+
+
+#endif
---
+++ logicadenegocios/local.c
@@ -0,0 +1,15 @@
+#include "../headers/clientes.h"
+#include "../headers/pedidos.h"
+#include "../headers/local.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#define CLEAR system("clear")
+
+Local* local = NULL;
+
+void cargarLocal(){
+    
+}
